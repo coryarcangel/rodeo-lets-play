4f24f3d46aaa3145de6d0dc58fe84431978d5dd1
Kevin Roark Jr <kevin.e.roark@gmail.com>
2018-03-28 17:27:11 -0700

    initial work on tensorflow, pillow, and tesseract code to process images into state

--- src/ai/state.py
+++ src/ai/state.py
@@ -1,7 +1,14 @@
 import tensorflow as tf
+import tesserocr
+from PIL import Image
 
-RawImageShape = [320, 640, 3]
+# Constants
+Iphone7PlusRawImageShape = [2208, 1242, 3]
+OutputImageSize = [160, 80] # width x height
 StateInputShape = [4]
+HudMenuHeight = 115 # pixels
+HudMenuPadding = 30 # pixels
+HudItemWidth = 240 # pixels
 
 class AIState():
     """
@@ -12,10 +19,16 @@ class AIState():
         * tappable_objects - list of (x,y,w,h) tuples
     """
     def __init__(self):
-        self.image = tf.placeholder(shape=RawImageShape, dtype=tf.uint8)
+        self.image = tf.placeholder(shape=Iphone7PlusRawImageShape, dtype=tf.uint8)
         self.money = 0
         self.stars = 0
         self.tappable_objects = []
+
+    def read_image_scores(self):
+        pass
+
+    def log(self):
+        pass
 
     def to_input(self):
         """
@@ -23,28 +36,87 @@ class AIState():
         """
         return []
 
-class AIStateProcessor():
+class AIGameplayImageProcessor():
     """
     Processes raw KK:H images into state.
     For now, resizes it and converts it to grayscale.
     In the future: use YOLO to translate image into object locations, and read known fixed-position HUD elements
     """
-    def __init__(self):
+    def __init__(self, image_shape=Iphone7PlusRawImageShape, output_image_size=OutputImageSize):
+        self.image_shape = image_shape
+        self.output_image_size = output_image_size
+
         # Build the Tensorflow graph
         with tf.variable_scope("state_processor"):
-            self.input_state = tf.placeholder(shape=RawImageShape, dtype=tf.uint8)
-            self.output = tf.image.rgb_to_grayscale(self.input_state)
-            self.output = tf.image.crop_to_bounding_box(self.output, 34, 0, 160, 160)
-            self.output = tf.image.resize_images(
-                self.output, [84, 84], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-            self.output = tf.squeeze(self.output)
+            self.input_image = tf.placeholder(shape=self.image_shape, dtype=tf.uint8)
 
-    def process(self, sess, state):
+            # Convert to grayscale
+            self.grayscale_image = tf.image.rgb_to_grayscale(self.input_image)
+
+            # Crop to non-menu area
+            image_width, image_height, _ = self.image_shape
+            self.output_image = tf.image.crop_to_bounding_box(self.grayscale_image, HudMenuHeight, 0, image_height - HudMenuHeight, image_width)
+
+            # Resize for performance
+            resize_method = tf.image.ResizeMethod.NEAREST_NEIGHBOR
+            self.output_image = tf.image.resize_images(self.output_image, self.output_image_size, method=resize_method)
+
+            # Remove 1-dimensional components
+            self.output_image = tf.squeeze(self.output_image)
+
+    def process_image(self, sess, image):
         """
         Args:
             sess: A Tensorflow session object
-            state: A [210, 160, 3] Atari RGB State
+            image: An image tensor with shape equal to `self.image_shape`
+        Returns:
+            Tuple of (output_image, grayscale_image)
+        """
+        # get processed image for gameplay area
+        sess.run(self.output_image, { self.input_image: image })
+        return self.output_image, self.grayscale_image
+
+class AIStateProcessor():
+    def process_from_file(self, sess, filename):
+        """
+        Args:
+            sess: A Tensorflow session object
+            filename: A string filepath to a png image of KK:H gameplay
         Returns:
             A processed AIState object.
         """
-        return sess.run(self.output, { self.input_state: state })
+
+        # Read image with pillow
+        image = Image.open(filename)
+
+        # Get shape
+        width, height = image.size
+        image_shape = (width, height, 3)
+
+        # get OCR text from known HUD elements
+        money_left = 0.53 * width
+        money_crop_box = (money_left, HudMenuPadding, money_left + HudItemWidth, HudMenuHeight - HudMenuPadding)
+        money_hud_image = image.crop(money_crop_box)
+        money_text = tesserocr.image_to_text(money_hud_image)
+
+        # Decode image for tensorflow
+        tf_image_file = tf.read_file(filename)
+        tf_image = tf.image.decode_png(tf_image_file)
+
+        # Process gameplay section with tensorflow
+        gameplay_image_processor = AIGameplayImageProcessor(image_shape=image_shape)
+        output_image, grayscale_image = gameplay_image_processor.process_image(sess, tf_image)
+
+
+def get_image_state(filename):
+    processor = AIStateProcessor()
+
+    with tf.Session() as sess:
+        tf.initialize_all_variables().run()
+
+        state = processor.process(sess, filename)
+        state.log()
+
+        return state
+
+get_image_state('src/img/ios_screenshot_1.png')
